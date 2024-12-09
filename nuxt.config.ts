50b60de748bc54d4e3c4bd8f4f8c8497743797c0
Otis Chen <variety928@gmail.com>
2024-12-09 22:34:08 +0800

    首頁 API

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  css: [
-    '@/assets/styles/all.scss', 
-  ]
-})
+  css: ["@/assets/styles/all.scss"],
+  runtimeConfig: {
+    token: "1234567890",
+    // 不公開的敏感訊息
+    public: {
+      apiUrl: "https://nuxr3.zeabur.app/api",
+      // PI URL 等環境差異有關的變數放 public
+    },
+  },
+});
